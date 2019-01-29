30458034dd38d7b14183402d8b0404ac79f5da8b
YuriiKhomenko <yuriikhomenkoukr@gmail.com>
2019-01-29 18:27:55 +0100

    Added additional checks to the programm

--- word_finder.py
+++ word_finder.py
@@ -4,10 +4,13 @@ from difflib import get_close_matches
 data = json.load(open("data.json", "r"))
 
 def word_finder(word):
-  word = word.lower()
-  if word in data:
-    return data[word]
-  elif len(get_close_matches(word, data.keys())) >= 0:
+  if word.title() in data:  # in case the word starts with capital letter
+    return data[word.title()]
+  elif word.lower() in data:
+    return data[word.lower()]
+  elif word.upper() in data: # in case user enters acronyms (USA, NATO, EU etc)
+    return data[word.upper()]
+  elif len(get_close_matches(word, data.keys())) >= 0: # in case we do not find exact word but are able to find words which are similar
     word = get_close_matches(word, data.keys())[0]
     choice = input("Did you mean {}? Press Y/N: ".format(word))
     choice = choice.lower()
@@ -16,7 +19,7 @@ def word_finder(word):
     elif choice == "no" or choice == "n":
       return "See you next time :)"
   else:
-    return "We didn't find such word. Try another one"
+    return "We didn't find such word. Try another one" # we didn't find any word that matches with the input
 
 word = input("Please enter a word: ")
 
